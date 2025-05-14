c35e55e68dbc1ad07c7ceb2711ad6f2f9b36ad0c
Bianca Angotti <bianca.angotti@gmail.com>
2025-05-13 21:54:00 -0600

    fix: new test of packages

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+// Export all components you want to make available in your package
+export { PatientHistory } from './PatientHistory';
+export { PatientOverview } from './PatientOverview';
+export { ThemeToggle } from './ThemeToggle';
+export { Timeline } from './Timeline';
---
+++ vite.lib.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.ts'),
+      name: 'MedplumComponents',
+      formats: ['es', 'umd'],
+      fileName: (format) => `medplum-components.${format}.js`
+    },
+    rollupOptions: {
+      // Make sure to externalize deps that shouldn't be bundled
+      external: ['react', 'react-dom', '@medplum/core', '@medplum/react', '@mantine/core'],
+      output: {
+        // Provide global variables to use in the UMD build
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@medplum/core': 'MedplumCore',
+          '@medplum/react': 'MedplumReact',
+          '@mantine/core': 'Mantine'
+        }
+      }
+    }
+  }
+});
